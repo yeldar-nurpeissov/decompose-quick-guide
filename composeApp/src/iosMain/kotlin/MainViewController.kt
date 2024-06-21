105c88dd6d81449fe06b13a66f992646db400ab5
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-06-22 02:39:21 +0500

    Add ApplicationLifecycle for iOS and attaching the lifecycle to document for Web/Wasm

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,12 +1,12 @@
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.ApplicationLifecycle
 import presentation.root.DefaultRootComponent
 
 fun MainViewController() = ComposeUIViewController {
     val root = remember {
-        DefaultRootComponent(DefaultComponentContext(LifecycleRegistry()))
+        DefaultRootComponent(DefaultComponentContext(ApplicationLifecycle()))
     }
     App(rootComponent = root)
 }
--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -2,7 +2,10 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.resume
+import com.arkivanov.essenty.lifecycle.stop
 import kotlinx.browser.document
+import org.w3c.dom.Document
 import presentation.root.DefaultRootComponent
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -10,7 +13,27 @@ fun main() {
     val lifecycle = LifecycleRegistry()
     val root = DefaultRootComponent(DefaultComponentContext(lifecycle))
 
+    lifecycle.attachToDocument()
+
     ComposeViewport(document.body!!) {
         App(root)
     }
 }
+
+private fun LifecycleRegistry.attachToDocument() {
+    fun onVisibilityChanged() {
+        if (visibilityState(document) == "visible") {
+            resume()
+        } else {
+            stop()
+        }
+    }
+
+    onVisibilityChanged()
+
+    document.addEventListener(type = "visibilitychange", callback = { onVisibilityChanged() })
+}
+
+// Workaround for Document#visibilityState not available in Wasm
+@JsFun("(document) => document.visibilityState")
+private external fun visibilityState(document: Document): String
