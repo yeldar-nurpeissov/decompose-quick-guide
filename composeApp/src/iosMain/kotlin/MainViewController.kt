82e990e4383463266c405ac7bc3790e49d365077
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-06-23 00:16:44 +0500

    Refactoring variable placement

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,11 +2,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.ApplicationLifecycle
-
-private val rootComponentFactory = DependencyInjection.rootComponentFactory
+import presentation.root.RootComponent
 
 fun MainViewController() = ComposeUIViewController {
     val rootComponent = remember {
+        val rootComponentFactory: RootComponent.Factory = DependencyInjection.rootComponentFactory
         rootComponentFactory(DefaultComponentContext(ApplicationLifecycle()))
     }
     App(rootComponent = rootComponent)
