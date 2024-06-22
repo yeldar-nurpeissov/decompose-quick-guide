0eb8d2391353d7053594f4339f73a92bc028c69e
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-06-22 22:12:04 +0500

    Add DependencyInjection

--- composeApp/src/androidMain/kotlin/yeldar/nurpeissov/decompose/quick/guide/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/yeldar/nurpeissov/decompose/quick/guide/MainActivity.kt
@@ -1,19 +1,22 @@
 package yeldar.nurpeissov.decompose.quick.guide
 
 import App
+import DependencyInjection
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.arkivanov.decompose.defaultComponentContext
-import presentation.root.DefaultRootComponent
 
 class MainActivity : ComponentActivity() {
+
+    val rootComponentFactory = DependencyInjection.rootComponentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Always create the root component outside Compose on the main thread
-        val rootComponent = DefaultRootComponent(defaultComponentContext())
+        val rootComponent = rootComponentFactory(defaultComponentContext())
 
         setContent {
             App(rootComponent = rootComponent)
