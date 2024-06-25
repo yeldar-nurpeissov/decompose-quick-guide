1f2ce239a20f679bfb1e495627361d2da2c2cba8
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-06-25 15:29:15 +0500

    Remove Kodein DI modules

--- composeApp/src/commonMain/kotlin/DependencyInjection.kt
+++ composeApp/src/commonMain/kotlin/DependencyInjection.kt
@@ -1,6 +1,32 @@
+import data.repository.DefaultPostRepository
+import data.repository.PostRepository
 import org.kodein.di.DI
-import presentation.root.rootFeatureModule
+import org.kodein.di.bindSingleton
+import org.kodein.di.instance
+import presentation.detail.DefaultDetailComponent
+import presentation.detail.DetailComponent
+import presentation.list.DefaultListComponent
+import presentation.list.ListComponent
+import presentation.root.DefaultRootComponent
+import presentation.root.RootComponent
 
 val kodeinDI = DI {
-    importOnce(rootFeatureModule)
+    bindSingleton<PostRepository> { DefaultPostRepository() }
+
+    bindSingleton<DetailComponent.Factory> {
+        DefaultDetailComponent.Factory(
+            repository = instance(),
+        )
+    }
+    bindSingleton<ListComponent.Factory> {
+        DefaultListComponent.Factory(
+            repository = instance(),
+        )
+    }
+    bindSingleton<RootComponent.Factory> {
+        DefaultRootComponent.Factory(
+            detailComponentFactory = instance(),
+            listComponentFactory = instance(),
+        )
+    }
 }
