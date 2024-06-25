e70a4f4d88a021d760bb14b5b676e0a6d6dd847c
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-06-25 15:43:28 +0500

    Remove Koin DI modules

--- composeApp/src/commonMain/kotlin/DependencyInjection.kt
+++ composeApp/src/commonMain/kotlin/DependencyInjection.kt
@@ -1,12 +1,41 @@
+import data.repository.DefaultPostRepository
+import data.repository.PostRepository
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
-import presentation.root.rootFeatureModule
+import org.koin.dsl.module
+import presentation.detail.DefaultDetailComponent
+import presentation.detail.DetailComponent
+import presentation.list.DefaultListComponent
+import presentation.list.ListComponent
+import presentation.root.DefaultRootComponent
+import presentation.root.RootComponent
 
 val koin by lazy { initKoin().koin }
 
 fun initKoin(appDeclaration: KoinAppDeclaration? = null) = startKoin {
     appDeclaration?.invoke(this)
-    modules(appModules())
+    modules(appModule)
 }
 
-fun appModules() = listOf(rootFeatureModule)
+val appModule = module {
+    single<PostRepository> { DefaultPostRepository() }
+
+    single<DetailComponent.Factory> {
+        DefaultDetailComponent.Factory(
+            repository = get(),
+        )
+    }
+
+    single<ListComponent.Factory> {
+        DefaultListComponent.Factory(
+            repository = get(),
+        )
+    }
+
+    single<RootComponent.Factory> {
+        DefaultRootComponent.Factory(
+            detailComponentFactory = get(),
+            listComponentFactory = get(),
+        )
+    }
+}
--- composeApp/src/commonMain/kotlin/data/di/PostRepositoryModule.kt
+++
@@ -1,9 +0,0 @@
-package data.di
-
-import data.repository.DefaultPostRepository
-import data.repository.PostRepository
-import org.koin.dsl.module
-
-val postRepositoryModule = module {
-    single<PostRepository> { DefaultPostRepository() }
-}
