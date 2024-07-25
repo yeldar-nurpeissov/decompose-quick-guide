978a83e49dc534320248904d071030a19677953c
Yeldar Nurpeissov <yeldar.nurpeissov@gmail.com>
2024-07-26 00:04:16 +0500

    Add StoreExt.kt

---
+++ composeApp/src/commonMain/kotlin/util/StoreExt.kt
@@ -0,0 +1,18 @@
+package util
+
+import com.arkivanov.decompose.Cancellation
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.mvikotlin.core.store.Store
+
+fun <T : Any> Store<*, T, *>.asValue(): Value<T> =
+    object : Value<T>() {
+        override val value: T get() = state
+
+        override fun subscribe(observer: (T) -> Unit): Cancellation {
+            val disposable = states(com.arkivanov.mvikotlin.core.rx.observer(onNext = observer))
+
+            return Cancellation {
+                disposable.dispose()
+            }
+        }
+    }
